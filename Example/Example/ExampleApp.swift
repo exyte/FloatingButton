06a74fa2db1529907259b1b2138804ecd2f59670
Daniil Manin <manindaniil@gmail.com>
2021-09-27 17:01:16 +0700

    Update code Update example Remove unneeded anyviews, selfs

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Daniil Manin on 27.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
